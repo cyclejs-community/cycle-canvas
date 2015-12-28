860b06d71087050d0e33d2718cd5ecb8742be5a5
Nick Johnstone <ncwjohnstone@gmail.com>
2015-12-28 17:56:56 +1300

    Pass isolate into cycle-restart

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import {run} from '@cycle/core';
 import {makeDOMDriver} from '@cycle/dom';
 import restart from 'cycle-restart';
+import isolate from '@cycle/isolate';
 
 var app = require('./src/app').default;
 
@@ -14,6 +15,6 @@ if (module.hot) {
   module.hot.accept('./src/app', () => {
     app = require('./src/app').default;
 
-    restart(app, sources, drivers);
+    restart(app, sources, drivers, isolate);
   });
 }
